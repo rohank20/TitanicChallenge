2e06fd59cfb8024ad35c1201c3ca062035cef15d
rohank20 <84909641+rohank20@users.noreply.github.com>
2021-10-28 10:27:42 +0200

    Added median age data

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ test = pd.read_csv("test.csv")
 train = train[['PassengerId', 'Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
 train_cleaned = train.dropna(axis = 0)
 train_cleaned = train_cleaned.replace(['male', 'female', 'S', 'Q', 'C'], [0, 1, 1, 2,3])
+
+male_age = train_cleaned.Age[train_cleaned['Sex'] == 0]
+female_age = train_cleaned.Age[train_cleaned['Sex'] == 1]
+
+male_median = male_age.median()
+female_median = female_age.median()
+mean_age = (male_median + female_median)/2
+
+train_cleaned.Age = train_cleaned.Age.fillna(value = mean_age)
+
 X = train_cleaned[['PassengerId', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
 y = train_cleaned[['Survived']]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -30,8 +40,16 @@ print(score)
 #Validation Data processing
 validation = test[['PassengerId', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
 validation.Fare.fillna(35.627188, inplace = True)
-validation_cleaned = validation.dropna(axis = 0)
-validation_data = validation_cleaned.replace(['male', 'female', 'S', 'Q', 'C'], [0, 1, 1, 2,3])
+#validation_cleaned = validation.dropna(axis = 0)
+validation_data = validation.replace(['male', 'female', 'S', 'Q', 'C'], [0, 1, 1, 2,3])
+
+val_male_age = validation_data.Age[validation_data['Sex'] == 0]
+val_female_age = validation_data.Age[validation_data['Sex'] == 1]
+
+val_male_median = val_male_age.median()
+val_female_median = val_female_age.median()
+val_mean_age = (val_male_median + val_female_median)/2
+validation_data.Age = validation_data.Age.fillna(value = val_mean_age)
 #test_data = test_cleaned[['PassengerId', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
 
 #Validation Data Prediction
